78339a4270d59266513f3289f0112b95ce983fd9
Abdelrahmanr000 <boda3472938@gmail.com>
2026-01-31 03:12:39 +0200

    Add SQL query to analyze sales over time

--- Scripts/Analyze_Sales_Over_Time.sql
+++ Scripts/Analyze_Sales_Over_Time.sql
@@ -1,1 +1,14 @@
-
+select
+	year(order_date) as OrderYear ,
+	month(order_date) as OrderMonth ,
+	sum(sales_amount) as Total_Sales ,
+	count(Distinct customer_key) as Total_Customers ,
+	sum(quantity) as Total_Quantity
+from
+	FactSales
+where
+	order_date is not null
+group by
+	year(order_date) , month(order_date)
+order by
+	year(order_date) , month(order_date);
